44208fa559223441f8447181806e039aebe63590
DrGaud <drgaud@hotmail.com>
2021-01-16 19:34:20 +0000

    update sw.js
    Working on some bugs in the service worker

--- projects/Asteroids Game/sw.js
+++ projects/Asteroids Game/sw.js
@@ -7,7 +7,10 @@ let urlsToCache = [
     // Relative Url Paths for our cache files.
     "./Asteroids.html",
     "./asteroidsGame.js",
-    "./asteroidsStylesheet.css"
+    "./asteroidsStylesheet.css",
+    "./assets/fonts/hyperspace/Hyperspace.otf",
+    "./assets/fonts/Trispace/Trispace-VariableFont_wdth,wght.ttf",
+    "./assets/graphics/asteroids-arcade-game-logo-sticker.svg"
 ]
 
 // PWA Install Event
@@ -16,9 +19,12 @@ self.addEventListener('install',(event)=>{
     // Perform install steps 
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache)=>{
+        .then(function(cache){
             console.log('Opened Cache')
             return cache.addAll(urlsToCache)
+        })
+        .catch(error=>{
+            console.log('Error Trying to install files to cache: ',error)
         })
     )
 })
@@ -28,12 +34,29 @@ self.addEventListener('install',(event)=>{
 self.addEventListener('fetch',(event)=>{
     event.respondWith(
         caches.match(event.request)
-        .then((response)=>{
-            // Cache Hit - return response
+        .then(response =>{
             if(response){
                 return response
             }
+
             return fetch(event.request)
+                   .then(response=>{
+                    //    Check if we received a valid response
+                    if(!response || response.status !== 200 || response.type !== 'basic'){
+                       return response
+                    }
+
+                    let responseToCache = response.clone()
+
+                    caches.open(CACHE_NAME)
+                        .then(cache =>{
+                            cache.put(event.request, responseToCache)
+                        }).catch(error=>console.log('Error Trying to cache response ',error))
+                    return response
+                   }).catch(error=>console.log('Error Trying to receive response for fetch event ', error))
+        })
+        .catch(error=>{
+            console.log('Error trying to fetch the files ', error);
         })
     )
 })
